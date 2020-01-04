8cc571d537d641e8cb09a4298f5065b6e3114b6f
= <sudo1701@gmail.com>
2020-01-04 17:35:46 +0000

    Moved message handling to it's own function

--- src/jobs/raid-event-handler.js
+++ src/jobs/raid-event-handler.js
@@ -21,70 +21,7 @@ class RaidEventHandler extends Job {
         const embed = this._createEventRichEmbed(new Date());
             
         channel.send(embed)
-               .then((message) => {
-                   //Bot does initial reactions so people can just click
-                   message.react(TICK_EMOJI)
-                          .then(() => {
-                              message.react(CROSS_EMOJI);
-                          }); //exploiting the promise to format the reactions in the right order
-
-                   //cache signups and unsign ids
-                   message.signUps = [];
-                   message.unsigns = [];
-
-                   //set up collector for raider signs
-                   let collector = message.createReactionCollector(
-                       this._reactionFilter, //filter function for reactions (don't want non raiders)
-                       { time: 15000 }
-                   );
-
-                   collector.on('collect', (reaction) => {
-                       let emoji = reaction.emoji.name;
-                       let user = reaction.users.last();
-
-                       //Ignore slackbot and other bots
-                       if (user.bot) {
-                           return;
-                       }
-
-                       if (emoji == TICK_EMOJI) {
-                           //check if they have already signed
-                           if (message.signUps.indexOf(user.id) != -1) {
-                               return;
-                           }
-
-                           //check if they unsigned, if so remove from unsigned list
-                           let unsignKey = message.unsigns.indexOf(user.id);
-
-                           if (unsignKey > -1) {
-                               message.unsigns.splice(unsignKey, 1);
-                           }
-
-                           message.signUps.push(user.id);
-                           this.handleSign(user);
-                       } else if (emoji == CROSS_EMOJI) {
-                           //check if they have already unsigned
-                           if (message.unsigns.indexOf(user.id) != -1) {
-                               return;
-                           }
-
-                           //check if they signed, if so remove from signed list
-                           let signKey = message.signUps.indexOf(user.id);
-
-                           if (signKey > -1) {
-                               message.signUps.splice(signKey, 1);
-                           }
-
-                           message.unsigns.push(user.id);
-                           this.handleUnsign(user);
-                       }
-                   });
-
-                   collector.on('end', () => {
-                       console.log("end");
-                       //TODO: do we clear up events here?
-                   });
-               })
+               .then(this._handleEventMessage.bind(this))
                .catch(console.error);
 
         this.processed = true;
@@ -110,6 +47,71 @@ class RaidEventHandler extends Job {
                 { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
             ))
             .setFooter(eventMeta.description);
+    }
+
+    _handleEventMessage(message) {
+        //Bot does initial reactions so people can just click
+        message.react(TICK_EMOJI)
+               .then(() => {
+                   message.react(CROSS_EMOJI);
+               }); //exploiting the promise to format the reactions in the right order
+
+        //cache signups and unsign ids
+        message.signUps = [];
+        message.unsigns = [];
+
+        //set up collector for raider signs
+        let collector = message.createReactionCollector(
+            this._reactionFilter, //filter function for reactions (don't want non raiders)
+            { time: 15000 }
+        );
+
+        collector.on('collect', (reaction) => {
+            let emoji = reaction.emoji.name;
+            let user = reaction.users.last();
+
+            //Ignore slackbot and other bots
+            if (user.bot) {
+                return;
+            }
+
+            if (emoji == TICK_EMOJI) {
+                //check if they have already signed
+                if (message.signUps.indexOf(user.id) != -1) {
+                    return;
+                }
+
+                //check if they unsigned, if so remove from unsigned list
+                let unsignKey = message.unsigns.indexOf(user.id);
+
+                if (unsignKey > -1) {
+                    message.unsigns.splice(unsignKey, 1);
+                }
+
+                message.signUps.push(user.id);
+                this.handleSign(user);
+            } else if (emoji == CROSS_EMOJI) {
+                //check if they have already unsigned
+                if (message.unsigns.indexOf(user.id) != -1) {
+                    return;
+                }
+
+                //check if they signed, if so remove from signed list
+                let signKey = message.signUps.indexOf(user.id);
+
+                if (signKey > -1) {
+                    message.signUps.splice(signKey, 1);
+                }
+
+                message.unsigns.push(user.id);
+                this.handleUnsign(user);
+            }
+        });
+
+        collector.on('end', () => {
+            console.log("end");
+            //TODO: do we clear up events here?
+        });
     }
 
     _reactionFilter(reaction, user) {
